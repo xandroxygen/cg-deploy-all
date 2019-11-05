0094c47b219ec84563c50b7f6e00558c53a41d5e
Xander Moffatt <xmoffatt@instructure.com>
2019-11-05 12:14:37 -0700

    add some prompts

--- lib/cg_deploy.rb
+++ lib/cg_deploy.rb
@@ -12,9 +12,22 @@ module CgDeploy
       Checks.run_all
       config = Config.new
       envs = options[:e]
+      tag = options[:t]
+      release = options[:r]
 
       envs_to_deploy = Envs.build_envs(config.get_environments, envs)
+
+      puts "\n*** Matched Environments ***"
       puts envs_to_deploy
+      
+      puts "\nContinue? (y/n)"
+      confirmation = gets.strip
+      if confirmation != 'y'
+        puts "...exiting without deploying"
+        return
+      else
+        puts "...deploying"
+      end
     end
   end
 end
